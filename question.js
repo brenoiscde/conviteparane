bbfea9400d2104c6794ef6c043927df620afba2a
brenoiscde <breninhoponticelli@gmail.com>
2025-04-14 22:39:59 -0300

    quase pronto

--- question.js
+++ question.js
@@ -1,14 +1,34 @@
-const botao = document.getElementById("button-nao")
+const botao = document.getElementById("button-nao");
+const carta = document.querySelector('.img-convite');
+const cartaAberta = document.querySelector('.pergunta');
+const respostanegativa = document.querySelector('.resposta-negativa')
+const area = document.querySelector('.area');
+const resposta = document.querySelector('.resposta');
 botao.addEventListener("mouseenter", () =>{
-    const larguraJanela = window.innerWidth;
-    const alturaJanela = window.innerHeight;
 
-    const maxX = larguraJanela - botao.offsetWidth;
-    const maxY = alturaJanela - botao.offsetHeight;
 
-    const posX = Math.floor(Math.random() * maxX);
-    const posY = Math.floor(Math.random() * maxY);
+  const posX = Math.floor(Math.random() * 900);
+  const posY = Math.floor(Math.random() * 600);
 
-    botao.style.left = `${posX}px`;
-    botao.style.top = `${posY}px`;
+  botao.style.left = `${posX}px`;
+  botao.style.top = `${posY}px`;
 });
+
+carta.addEventListener('click', ()=>{
+    cartaAberta.style.display = "flex";
+    carta.style.display = "none"
+})
+
+const botaoSim = document.getElementById("button-sim");
+const container = document.querySelector(".coracoes-container");
+
+botaoSim.addEventListener("click", () => {
+    cartaAberta.style.display = "none"
+    resposta.style.display = "flex"
+   
+});
+
+botao.addEventListener('click', ()=>{
+    cartaAberta.style.display = "none"
+    respostanegativa.style.display = "flex"
+})
